5a0f3d4f150cc66f783a55f60aa9b4c97db43c19
Kiet Huynh <kiet.huynh@mercatus.com>
2025-09-26 21:21:33 +0700

    resolve error

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -12,10 +12,14 @@ export async function generateMetadata({
   return { title: `Post: ${params.slug}` };
 }
 
-export default function Page({ params }: PageProps) {
+export default async function Page({ params }: PageProps) {
+  const parsedParams = await params;
+
+  console.log("parsed params");
+
   return (
     <div>
-      <h1>Slug: {params.slug}</h1>
+      <h1>Slug: {params?.slug}</h1>
       <p>Kiet Huynh</p>
       <p>Welcome to nextJS</p>
     </div>
